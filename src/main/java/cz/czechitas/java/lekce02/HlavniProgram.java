543cb69b02f5eb3b43c9feff440690f2b6c9fbf5
AdelaLindner <a.lindnerova@seznam.cz>
2023-02-14 20:19:17 +0100

    Domeček a prasátko

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -7,7 +7,41 @@ public class HlavniProgram {
     public void start() {
         zofka = new Turtle();
 
-        //TODO tady bude kód
+        for (int i = 0; i < 4; i++) {
+            zofka.turnRight(90);
+            zofka.move(100);
+        }
+        for (int i = 0; i < 2; i++) {
+            zofka.turnRight(60);
+            zofka.move(60);
+        }
+
+        zofka.turnLeft(30);
+        zofka.penUp();
+        zofka.move(200);
+        zofka.penDown();
+
+
+
+        for (int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnRight(90);
+        }
+        zofka.turnLeft(240);
+        zofka.move(60);
+        zofka.turnLeft(60);
+        zofka.move(80);
+        zofka.turnLeft(180);
+        zofka.move(20);
+        zofka.turnLeft(105);
+        zofka.move(20);
+
+
+
+
+
+
+
     }
 
     public static void main(String[] args) {
